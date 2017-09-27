d55051a5384ce7b9cf68bf071e8d7cb8d18acc8f
Nancy <belscher@Nancys-MacBook-Pro.local>
2017-09-27 13:58:24 -0400

    answers index and answers show working

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,21 @@
 class AnswersController < ApplicationController
 
+
+  def index
+    # question_answers GET    /questions/:question_id/answers(.:format)     answers#index
+    @question = Question.find(params[:question_id])
+  end
+
   def show
+    @question = Question.find(params[:question_id])
     @answer = Answer.find(params[:id])
+
   end
 
   def create
+    # @answer = @question.answers.build(answer_params)
+    # @answer.user = current_user
+    # if @answer.save ...
     answer = Answer.new(answer_params)
     answer.question_id = params[:question_id]
     answer.save
--- app/views/answers/index.json.jbuilder
+++ app/views/answers/index.json.jbuilder
@@ -2,9 +2,9 @@ json.question do
   json.id @question.id
   json.url question_url(@question)
   json.title @question.title
-  # json.answers @question.answers do |answer|
-    json.body @answer.body
+  json.answers @question.answers do |answer|
+    json.body answer.body
     # json.url author_url(author)
     # json.name author.name
-  # end
+  end
 end
